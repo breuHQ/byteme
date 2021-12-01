718318cfc365eeb63742270d7745235979ab043f
Yousuf Jawwad <yousuf@maher.pk>
2021-12-01 20:26:00 +0500

    chore(): added .editorconfig & .rustfmt.toml

---
+++ src/constants.rs
@@ -0,0 +1,4 @@
+pub const ERROR_STRUCT_NAMED_FIELD: &str = "`ByteMe` only works for a struct with named fields";
+pub const ERROR_ATTRIBUTE_ARGS_LENGTH: &str = "`#[byte_me()]` can only define one argument";
+pub const ERROR_ATTRIBUTE_DATA_TYPE: &str = "`#[byte_me()]` can only be used with `u8`, `u16`, `u32`, `u64` & `u128`";
+pub const ERROR_FIELD_ARRAY_DATA_TYPE: &str = "Array field is only supported for `u8`";
